9876db9095f08a70c32fc87bde576e0486f17361
DIR\a.carmona.garrido <ansuncg@gmail.com>
2023-03-31 10:33:44 +0200

    Segundo commint Asun

---
+++ src/main/java/es/lassotero/asun/ClaseSuma.java
@@ -0,0 +1,12 @@
+package es.lassotero.asun;
+
+public class ClaseSuma {
+
+    public static int sumaNumeros (int numA, int numB) {
+        return numA + numB;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumaNumeros(2,3));
+    }
+}
